76586859b87a1c910962bcbecfe7ae9e931896c4
bayamako-afk <bayamako@gmail.com>
2026-03-04 05:52:50 -0500

    fix: 座布団をplayer-info-row背景に変更

--- guno_v5/src/js/ui_v405.js
+++ guno_v5/src/js/ui_v405.js
@@ -582,14 +582,17 @@ function renderAll() {
         }).join('');
         let stCount = 0;
         Object.values(mapState).forEach(owner => { if(owner === i) stCount++; });
-        const turnBanner = isTurn
+        const infoClass = isTurn ? 'player-info-row player-info-row--active' : 'player-info-row';
+        const turnLabel = isTurn
             ? (p.isHuman && !autoPlay
-                ? '<div class="turn-banner turn-banner--human">🎯 ' + t('あなたの番です！', 'Your turn!') + '</div>'
-                : '<div class="turn-banner turn-banner--cpu">⏳ ' + p.icon + ' ' + p.name + t('の番', '\'s turn') + '</div>')
+                ? ' <span class="turn-label turn-label--human">🎯 ' + t('あなたの番', 'Your turn') + '</span>'
+                : ' <span class="turn-label turn-label--cpu">⏳ ' + t('番', 'turn') + '</span>')
             : '';
         return '<div class="player-box ' + (isTurn?'active-turn':'') + ' ' + (p.status==='eliminated'?'eliminated':'') + 
-               '" style="border-left-color:' + p.color + '"><b>' + p.icon + ' ' + p.name + 
-               '</b> <small>Stations:' + stCount + ' | GUNO:' + p.guno + ' | Score:' + calculateScore(i) + '</small>' + turnBanner + '<br>' + cardsHtml + '</div>';
+               '" style="border-left-color:' + p.color + '">'
+               + '<div class="' + infoClass + '"><b>' + p.icon + ' ' + p.name + 
+               '</b> <small>Stations:' + stCount + ' | GUNO:' + p.guno + ' | Score:' + calculateScore(i) + '</small>' + turnLabel + '</div>'
+               + '<div class="player-cards-row">' + cardsHtml + '</div></div>';
     }).join('');
 
     updateStationNodeIcons();
